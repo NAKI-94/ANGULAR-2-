396f8f1064b60b976e6f8613e86064c70f7ca8a0
Naki-94 <quina.dev94@gmail.com>
2024-08-08 20:34:40 -0500

    ejercicios

---
+++ src/app/signal/signal-ejemplo2/signal-ejemplo2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-ejemplo2',
+  standalone: true,
+  imports: [],
+  templateUrl: './signal-ejemplo2.component.html',
+  styleUrl: './signal-ejemplo2.component.scss'
+})
+export class SignalEjemplo2Component {
+
+}
---
+++ src/app/signal/signal-routings.ts
@@ -0,0 +1,28 @@
+import { Route } from "@angular/router";
+import { SignalComponent } from "./signal.component";
+
+const ROUTES:Route[]=[
+    {
+        path:"",
+        component: SignalComponent,
+        children:[
+            {
+                path:"",
+                pathMatch:"full",
+                redirectTo:"signal-ejemplo1"
+            },
+            {
+                path:"signal-ejemplo1",
+                loadComponent:()=>
+                    import("./signal-ejemplo1/signal-ejemplo1.component")
+            },
+            {
+                path:"signal-ejemplo3",
+                loadComponent:()=>
+                    import("./signal-ejemplo3/signal-ejemplo3.component")
+            }
+        ]
+    }
+]
+
+export default ROUTES;
